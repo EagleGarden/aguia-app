b84f5e1f2046c4b264789444084b7ee7e6cd22d8
EagleGarden <aguia.jardinagem.nv@gmail.com>
2025-11-15 16:58:56 -0400

    criação do hook e importação dele para a tela de início

--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -2,15 +2,30 @@ import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
 import { Link } from 'expo-router'
 import { Text, View } from 'react-native'
 import { SignOutButton } from '@/components/SignOutButton'
+import { useAgendamento } from '../../hooks/useAgendamento'
+import { useEffect } from 'react'
 
 export default function Page() {
   const { user } = useUser()
+  const { agendamentos, summary, isLoading, loadData, deleteTransaction } = useAgendamento(user.id)
+
+  useEffect(() => {
+    loadData()
+  }, [loadData])
+
+  console.log('user', user?.emailAddresses[0].emailAddress)
+  console.log('agendamentos:', agendamentos)
+  console.log('summary:', summary)
+
 
   return (
     <View>
       <SignedIn>
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
         <SignOutButton />
+        <Text>Resumo:</Text>
+        <Text>Concluídos: {summary.concluidos} (Valor: {summary.valor_total}) </Text>
+        <Text>Agendados: {summary.agendados}</Text>
       </SignedIn>
       <SignedOut>
         <Link href="/(auth)/sign-in">
---
+++ mobile/hooks/useAgendamento.js
@@ -0,0 +1,66 @@
+import { useCallback, useState } from "react";
+import { Alert } from 'react-native';
+
+const API_URL = "http://localhost:8000/api";
+
+export const useAgendamento = (servicoTipoId) => { 
+    const [agendamentos, setAgendamentos] = useState([]);
+    const [summary, setSummary] = useState({
+        concluidos: 0,
+        valor_total: 0,
+        agendados: 0,
+        andamento: 0,
+    });
+    const [isLoading, setIsLoading] = useState(true);
+
+    const fetchSummary = useCallback(async () => {
+        try {
+            const response = await fetch(`${API_URL}/agendamento/summary/geral`);
+            const data = await response.json();
+            setSummary(data);
+        } catch (error) {
+            console.error("Error fetching summary:", error);
+        }
+    }, []);
+
+    const fetchAgendamentos = useCallback(async () => {
+        try {
+            const url = servicoTipoId
+                ? `${API_URL}/agendamento/${servicoTipoId}`
+                : `${API_URL}/agendamento`;
+
+            const response = await fetch(url);
+            const data = await response.json();
+            setAgendamentos(data);
+        } catch (error) {
+            console.error("Error fetching agendamentos:", error);
+        }
+    }, [servicoTipoId]);
+
+    const loadData = useCallback(async () => {
+        
+        setIsLoading(true);
+        try {
+            await Promise.all([fetchAgendamentos(), fetchSummary()]);
+        } catch (error) {
+            console.error('Error loading data:', error)
+        } finally {
+            setIsLoading(false);
+        }
+    }, [fetchAgendamentos, fetchSummary]);
+
+    const deleteAgendamento = async (id) => {
+        try {
+            const response = await fetch(`${API_URL}/agendamento/${id}`, { method: 'DELETE' });
+            if (!response.ok) throw new Error('Failed to delete agendamento');
+            
+            loadData();
+            Alert.alert('Sucesso', 'Agendamento deletado com sucesso');
+        } catch (error) {
+            console.error('Error deleting agendamento:', error);
+            Alert.alert('Error', 'Falha ao deletar agendamento');
+        }
+    }
+
+    return { agendamentos, summary, isLoading, loadData, deleteAgendamento }
+}
